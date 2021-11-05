ba91d0e274acf9e37287c7f447a4166d3aaf7c0c
Peje <guillepazerrobidart@yahoo.com.ar>
2021-11-05 09:53:24 -0300

    modifique una nadita

--- script5.js
+++ script5.js
@@ -2,7 +2,7 @@
 // Ejercicio Nº5:
 // Crear una funcion que tome un array y lo invierta sin cambiar el original.
 
-function reverseArray(e){
-    return e.map(function reverse(v){
+function reverseArray(arr){
+    return arr.map(function reverse(v){
         return v.reverse()});
 }
